f50b89c650683248658348e382351b6c0e2d10b9
Roger Bixby <javafreek@spasticmonkeys.com>
2018-02-03 15:51:28 -0800

    More beginner's files.

---
+++ ex8.py
@@ -0,0 +1,7 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter, formatter, formatter, formatter)
+print(formatter.format("The words", "you say", "can never", "be true"))
